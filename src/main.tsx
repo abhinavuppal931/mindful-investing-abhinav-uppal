3fa19bf66394fcdfdb14a15f68467b005f8f1561
abhinavuppal931 <85035070+abhinavuppal931@users.noreply.github.com>
2025-04-19 01:43:24 -0400

    Delete main.tsx to fix Next.js conflict

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-// In development mode, render the App directly
-// In production with Next.js, _app.tsx will handle this
-createRoot(document.getElementById("root")!).render(<App />);
